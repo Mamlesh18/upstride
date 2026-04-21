a3eea122915ad345cd0f519053ee32ef11498d6a
Mamlesh18 <mamleshsurya6@gmail.com>
2026-04-21 11:54:38 +0530

    feat

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://upstride-backend-portal.vercel.app";
 
 function getToken(): string | null {
   return localStorage.getItem("token");
